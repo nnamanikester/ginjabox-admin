3cce77902452b43e604049c1b5c5151d576d5212
John Kester <nnamanikester@gmail.com>
2020-04-13 10:46:50 +0100

    Worked on Rejected stock log

--- src/pages/InventoryLog/RejectedStockLog.js
+++ src/pages/InventoryLog/RejectedStockLog.js
@@ -1,47 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { apiUrl } from "../../config";
+import axios from "axios";
+import Skeleton from "react-loading-skeleton";
 import {
   MDBCard,
   MDBCardBody,
   MDBContainer,
   MDBDataTable,
-  MDBView
+  MDBView,
+  MDBBadge
 } from "mdbreact";
-const data = {
-  columns: [
-    {
-      label: "S/N",
-      field: "sn",
-    },
-    {
-      label: "Name",
-      field: "name",
-    },
-    {
-      label: "Stock ID",
-      field: "stockId",
-    },
-    {
-      label: "Date",
-      field: "date",
-    }
-  ],
-  rows: [
-    {
-      sn: "1",
-      name: "John Kester",
-      stockId: "876yghsd7YU",
-      date: "2020/03/02"
-    },
-    {
-      sn: "2",
-      name: "Nwagu Victor",
-      stockId: "67829iweouso",
-      date: "2020/22/12"
-    },
-  ]
-};
-
 const RejectedStockLog = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [stocks, setStocks] = useState([]);
+
+  const data = {
+    columns: [
+      {
+        label: "S/N",
+        field: "sn",
+      },
+      {
+        label: "Name",
+        field: "name",
+      },
+      {
+        label: "Stock ID",
+        field: "stockId",
+      },
+      {
+        label: "Date",
+        field: "date",
+      }
+    ],
+    rows: !loading ? stocks : [{
+      sn: <Skeleton />,
+      id: <Skeleton />,
+      agentName: <Skeleton />,
+      agentPhone: <Skeleton />,
+      agentIdNo: <Skeleton />,
+      agentIdentification: <Skeleton />,
+      pickupDateFrom: <Skeleton />,
+      pickupDateTo: <Skeleton />,
+      status: <Skeleton />
+    }]
+  };
+
+
+  const loadOrders = async () => {
+    setLoading(true);
+    axios.get(`${apiUrl}/stocks`, {
+      headers: { "x-admin-auth": localStorage.getItem('token') }
+    })
+      .then(res => {
+        let sn = stocks.length;
+        const rows = res.data.data.map(order => {
+          const row = {
+            sn: sn + 1,
+            id: order.id,
+            agentIdNo: order.pickupAgentIdNumber,
+            agentName: order.pickupAgentName,
+            agentPhone: order.pickupAgentPhone,
+            agentIdentification: order.pickupAgentIdentification,
+            pickupDateFrom: order.pickupDate.min,
+            pickupDateTo: order.pickupDate.max,
+            status: order.status === 2 ? <MDBBadge color="success">Success</MDBBadge> : <MDBBadge className="danger-color">Failed</MDBBadge>,
+          };
+          sn++;
+          return row;
+        })
+        setLoading(false);
+        setStocks(rows);
+      })
+      .catch(err => {
+        console.log(err);
+        return [];
+      })
+  }
+
+  useEffect(() => {
+    loadOrders();
+  }, []);
+
+
   return (
     <MDBContainer>
       <MDBCard>
